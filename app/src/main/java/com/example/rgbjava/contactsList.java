1c9f001fe2372f1c8601531c41486c3e4382198b
Degik <prodax9900@gmail.com>
2022-07-05 01:03:33 +0200

    Updated contactsList Updated addContact Updated xml for addContact

--- app/src/main/java/com/example/rgbjava/contactsList.java
+++ app/src/main/java/com/example/rgbjava/contactsList.java
@@ -2,6 +2,7 @@ package com.example.rgbjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -21,11 +22,9 @@ public class contactsList extends AppCompatActivity {
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                openAdd();
             }
         });
-
-
 
         //ArrayAdapter<Contact> arrayAdapter = new ArrayAdapter<Contact>(this, R.layout.activity_contacts_list, MainActivity.user.getContacts());
         //ListView listView = (ListView) findViewById(R.id._dynamic);
@@ -35,6 +34,7 @@ public class contactsList extends AppCompatActivity {
     }
 
     public void openAdd(){
-
+        Intent intentOpenAdd = new Intent(this, addContact.class);
+        startActivity(intentOpenAdd);
     }
 }
