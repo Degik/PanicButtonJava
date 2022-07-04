f8b51423ad8428ec987f2e321a2397094f6f0248
Degik <prodax9900@gmail.com>
2022-07-04 23:29:41 +0200

    Updated contactsList Updated xml for insert textView

--- app/src/main/java/com/example/rgbjava/contactsList.java
+++ app/src/main/java/com/example/rgbjava/contactsList.java
@@ -3,20 +3,38 @@ package com.example.rgbjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class contactsList extends AppCompatActivity {
+
+    private Button buttonAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts_list);
 
+        buttonAdd = (Button) findViewById(R.id.addContact);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
         //ArrayAdapter<Contact> arrayAdapter = new ArrayAdapter<Contact>(this, R.layout.activity_contacts_list, MainActivity.user.getContacts());
         //ListView listView = (ListView) findViewById(R.id._dynamic);
         ContactsAdapter contactsAdapter = new ContactsAdapter(this, MainActivity.user.getContacts());
         ListView listView = (ListView) findViewById(R.id.contactsListView);
         listView.setAdapter(contactsAdapter);
+    }
+
+    public void openAdd(){
+
     }
 }
