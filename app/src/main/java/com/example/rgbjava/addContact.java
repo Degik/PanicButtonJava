fb5e5ad500d79dd7773be0c64f7481e7c6127eb0
Degik <prodax9900@gmail.com>
2022-07-05 16:05:14 +0200

    Updated contactsList Updated addContact Updated xml for addContact

--- app/src/main/java/com/example/rgbjava/addContact.java
+++ app/src/main/java/com/example/rgbjava/addContact.java
@@ -1,7 +1,11 @@
 package com.example.rgbjava;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +19,8 @@ public class addContact extends AppCompatActivity {
     private EditText editEmail;
     private EditText editPhone;
 
+    private Context context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +33,39 @@ public class addContact extends AppCompatActivity {
                 editFirstName = (EditText) findViewById(R.id.textEditFirstName);
                 editLastName = (EditText) findViewById(R.id.textEditLastName);
                 editEmail = (EditText) findViewById(R.id.editEmail);
-                editPhone = (EditText) findViewById(R.id)
-                openSave();
+                editPhone = (EditText) findViewById(R.id.editPhone);
+                if(!(emptyBox(editFirstName) || emptyBox(editLastName) || emptyBox(editEmail) || emptyBox(editPhone))){
+                    String firstName = editFirstName.getText().toString();
+                    String lastName = editLastName.getText().toString();
+                    String email = editEmail.getText().toString();
+                    String phone = editPhone.getText().toString();
+                    // Creo il contatto
+                    Contact c = new Contact(firstName, lastName, phone, email);
+                    // Aggiungo il contatto alla lista
+                    MainActivity.user.addContact(c);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setMessage("Successo").setTitle("Info");
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            openContactList();
+                        }
+                    })
+                }
             }
         });
     }
 
-    public void openSave(){
+    public void openContactList(){
+        Intent intentOpenContactList = new Intent(this, contactsList.class);
+        startActivity(intentOpenContactList);
+    }
 
+    public boolean emptyBox(EditText editText){
+        if(editText.getText().toString().equals("")){
+            return true;
+        }
     }
 }
