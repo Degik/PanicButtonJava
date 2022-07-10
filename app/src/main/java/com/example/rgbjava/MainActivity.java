21e3ec91cf951870ec7a8ee7504c6201e3a58104
Degik <prodax9900@gmail.com>
2022-07-10 18:55:13 +0200

    resolved picture bug

--- app/src/main/java/com/example/rgbjava/MainActivity.java
+++ app/src/main/java/com/example/rgbjava/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.rgbjava;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -21,11 +25,13 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.MapView;
 
@@ -41,15 +47,18 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private Button buttonContactsList;
     private Button buttonPanic;
+    private Button takePhotoButton;
     private Button settingsButton;
     private int PERMISSION_ID = 44;
+    private int PERMISSION_ID_CAMERA = 45;
     //
     public LocationManager locationManager;
     private Geo geo;
+    private String currentPhotoPath;
     //
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private Bitmap bitmapImage;
-    private File file;
+    private File file = null;
     //
     public static BackupFile backupFile;
     public static User user;
@@ -71,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
             backupFile.makeBackupContactsList();
             openFirstStep();
         } else {
-            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
             if(checkPerm()){
                 geo = new Geo(this, locationManager);
                 Thread threadGps = new Thread(geo);
@@ -107,13 +116,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
         buttonPanic = (Button) findViewById(R.id.panicButton);
+        buttonPanic.setEnabled(false);
         buttonPanic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startPanic();
+                //startPanic();
             }
         });
 
+        takePhotoButton = (Button) findViewById(R.id.buttonPhoto);
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkCameraPerm()){
+                    startIntentPicture();
+                    buttonPanic.setEnabled(true);
+                } else {
+                    requestCameraPerm();
+                }
+            }
+        });
     }
 
 
@@ -153,21 +175,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startPanic(){
-        startCamera();
-        sendEmail();
+        //sendEmail();
     }
 
-    private void startCamera(){
+    private void startIntentPicture(){
         Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        takePicture.putExtra("android.intent.extra.quickCapture", true);
         try{
-            startActivityForResult(takePicture, REQUEST_IMAGE_CAPTURE);
-        } catch(ActivityNotFoundException e){
+            file = createImageFile();
+        } catch (IOException e){
             e.printStackTrace();
+        }
+        if(file != null){
+            Uri photoUri = FileProvider.getUriForFile(this, "com.example.rgbjava.fileprovider", file);
+            takePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            startActivityForResult(takePicture, REQUEST_IMAGE_CAPTURE);
         }
     }
 
-    private void sendEmail(){
+    /*private void sendEmail(){
         ArrayList<String> list = new ArrayList<>();
         for(Contact c: contacts){
             list.add(c.getEmail());
@@ -181,13 +206,15 @@ public class MainActivity extends AppCompatActivity {
         //
         intentSendEmail.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
         startActivity(Intent.createChooser(intentSendEmail, "Test"));
-    }
+    }*/
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
-            bitmapImage = (Bitmap) data.getExtras().get("data");
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
+            Bundle extras = data.getExtras();
+            bitmapImage = (Bitmap) extras.get("data");
+            /*
             File myDir = new File(Environment.getExternalStorageDirectory() + "/picture");
             DateFormat format = new SimpleDateFormat("dd_MM_yyyy_H_mm_ss", Locale.getDefault());
             Date curDate = new Date();
@@ -204,7 +231,61 @@ public class MainActivity extends AppCompatActivity {
                 }
             } catch (IOException e){
                 e.printStackTrace();
+            }*/
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String time = new SimpleDateFormat("ddMMyyyy_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + time + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,".jpg", storageDir);
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private boolean checkCameraPerm(){
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestCameraPerm(){
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.CAMERA}, PERMISSION_ID_CAMERA);
+    }
+
+    /*
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if(requestCode == PERMISSION_ID){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "Permesso consentito", Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(getApplicationContext(), "Permesso negato", Toast.LENGTH_SHORT).show();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        showMessageOKCancel("Devi consentirmi di accedere alla tua fotocamera",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                            requestCameraPerm();
+                                        }
+                                    }
+                                });
+                    }
+                }
             }
         }
     }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("Acconsenti", okListener)
+                .setNegativeButton("Nega", null)
+                .create()
+                .show();
+    }*/
 }
